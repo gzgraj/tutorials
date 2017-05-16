0a6867a1fd64e56dc0f9b2f0d22bc9367a02801a
APRZECHO <APRZECHO@CE11106.corp.capgemini.com>
2017-05-16 15:27:33 +0200

    initial project

---
+++ simpleapp/src/test/java/hsbclearn/simpleapp/AppTest.java
@@ -0,0 +1,13 @@
+package hsbclearn.simpleapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+    
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
