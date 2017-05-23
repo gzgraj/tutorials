f0b5c871dda3a2a21516038e27be717336f0418d
APRZECHO <APRZECHO@CE11106.corp.capgemini.com>
2017-05-24 00:46:59 +0200

    JMS Example + pomki

--- simpleapp/src/main/java/hsbclearn/simpleapp/examples/ExampleServlet.java
+++ simpleapp/src/main/java/hsbclearn/simpleapp/examples/ExampleServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ExampleSRV")
+@WebServlet("/ExampleServlet")
 public class ExampleServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
